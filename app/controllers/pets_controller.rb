3afc075495656e3b3593e85ad50f5e7430f40714
Justin Pacubas <jpacubas@gmail.com>
2014-09-02 11:58:20 -0700

    Add method to create new pet and add input fields to index page.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,22 @@
 class PetsController < ApplicationController
   def index
     @pets = Pet.all
+    @pet = Pet.new
     render('pets/index.html.erb')
   end
 
   def show
     @pet = Pet.find(params[:id])
     render('pets/show.html.erb')
+  end
+
+  def create
+    @pet = Pet.new(params[:pet])
+    if @pet.save
+      render('pets/success.html.erb')
+    else
+      @pets = Pet.all
+      render('pets/index.html.erb')
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'pets#index'})
   match('pets', {:via => :get, :to => 'pets#index'})
+  match('pets', {:via => :post, :to => 'pets#create'})
+  match('pets/new', {:via => :get, :to => 'pets#index'})
   match('pets/:id', {:via => :get, :to => 'pets#show'})
 end
