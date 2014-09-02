480f130866cb73f920d5473d322774cf5d3f6f3b
Justin Pacubas <jpacubas@gmail.com>
2014-09-02 13:49:55 -0700

    Update veterinarians table column "phone" to string.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902181326) do
+ActiveRecord::Schema.define(version: 20140902204645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20140902181326) do
 
   create_table "veterinarians", force: true do |t|
     t.string   "name"
-    t.integer  "phone"
+    t.string   "phone"
     t.string   "email"
     t.string   "address"
     t.datetime "created_at"
