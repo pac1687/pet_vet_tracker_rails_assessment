10e9ee1af9e9e811a6766dddfb7e563d707da95d
Justin Pacubas <jpacubas@gmail.com>
2014-09-02 11:33:39 -0700

    Update routes & Pets controller, and write show view.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,9 @@ class PetsController < ApplicationController
     @pets = Pet.all
     render('pets/index.html.erb')
   end
+
+  def show
+    @pet = Pet.find(params[:id])
+    render('pets/show.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'pets#index'})
   match('pets', {:via => :get, :to => 'pets#index'})
+  match('pets/:id', {:via => :get, :to => 'pets#show'})
 end
